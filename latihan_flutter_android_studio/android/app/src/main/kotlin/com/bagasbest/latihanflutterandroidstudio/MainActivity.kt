dd72f921b9fc45b77916116a22a68701ff8907ef
bagasbest <bagasbest80@gmail.com>
2020-06-05 00:13:42 +0700

    belajar flutter with -  Erico Darmawan Handoyo

---
+++ latihan_flutter_android_studio/android/app/src/main/kotlin/com/bagasbest/latihanflutterandroidstudio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bagasbest.latihanflutterandroidstudio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ latihan_flutter_android_studio/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+//shortcut untuk membuat statelessWidget  = stl + Center
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // using material design
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasi Hello World"),
+        ),
+        body: Center(
+            child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 100,
+                child: Text(
+                  "Selamat datang dan Hello World!, saya sedang melatih kemampuan saya",
+                  // maxLines: 2,
+                  // overflow ellipsis memberikan ... pada screen yang menandakan ada lanjutan text yang terpotong
+                  // overflow clip tetap seperti maxLines
+                  // overflow: TextOverflow.clip,
+                  // softWrap: false,
+                  // textAlign: TextAlign.center,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ))),
+      ),
+    );
+  }
+}
