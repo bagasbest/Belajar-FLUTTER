b2806ab749c6633fea41435b10ae6f96fb6031c3
bagasbest <bagasbest80@gmail.com>
2020-06-05 23:21:10 +0700

    belajar text style

---
+++ pertemuan8_text_style/lib/main.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Pertemuan 8 Text Style"),
+        ),
+        body: Center(
+          child: Text(
+            "Ini adalah Text",
+            style: TextStyle(
+                fontFamily: "FiraCode",
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.overline,
+                decorationColor: Colors.red,
+                decorationThickness: 5,
+                decorationStyle: TextDecorationStyle.wavy),
+          ),
+        ),
+      ),
+    );
+  }
+}
