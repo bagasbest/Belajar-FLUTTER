1b5cd45b2048f027561d3522958d085f2b0f152e
bagasbest <bagasbest80@gmail.com>
2020-06-06 09:46:51 +0700

    belajar ListView

---
+++ pertemuan9_list_view/android/app/src/main/kotlin/com/example/pertemuan9_list_view/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pertemuan9_list_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ pertemuan9_list_view/lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Widget> widgets = [];
+  int cnt = 1;
+
+  // isi widget sebelum ditampilkan
+  // buat constructor
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Belajar List View"),
+        ),
+        body: ListView(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              RaisedButton(
+                child: Text("Tambah data"),
+                onPressed: () {
+                  setState(() {
+                    widgets.add(Text(
+                      "Data ke-" + cnt.toString(),
+                      style: TextStyle(fontSize: 35),
+                    ));
+                    cnt++;
+                  });
+                },
+              ),
+              RaisedButton(
+                  child: Text("Hapus data"),
+                  onPressed: () {
+                    setState(() {
+                      widgets.removeLast();
+                      cnt--;
+                    });
+                  })
+            ],
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: widgets,
+          )
+        ]),
+      ),
+    );
+  }
+}
