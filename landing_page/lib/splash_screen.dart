cb405cb9675fb4f3de656b335aee76ee02bde2db
bagasbest <bagasbest80@gmail.com>
2020-06-08 09:34:36 +0700

    landing_page

---
+++ landing_page/lib/splash_screen.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:landing_page/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    splashScreenStart();
+  }
+
+  splashScreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(child: Image.asset('assets/logo/logo-04.png')));
+  }
+}
