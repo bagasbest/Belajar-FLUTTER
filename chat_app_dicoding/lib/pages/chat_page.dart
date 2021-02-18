f82ad6872486abe912af0df4fd1adcedfb64dcf0
bagasbest <bagasbest80@gmail.com>
2021-02-18 07:17:18 +0700

    firestore

--- chat_app_dicoding/lib/pages/chat_page.dart
+++ chat_app_dicoding/lib/pages/chat_page.dart
@@ -1,11 +1,42 @@
+import 'package:chat_app_dicoding/widget/message_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'login_page.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   static const String id = 'chat_page';
+
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
   final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  User _activeUser;
+  
+  final _messageTextController = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      var currentUser = await _auth.currentUser;
+      if(currentUser != null) {
+        _activeUser = currentUser;
+      }
+    }catch (err) {
+      print(err);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,7 +58,42 @@ class ChatPage extends StatelessWidget {
         padding: EdgeInsets.all(8),
         child: Column(
           children: [
-            Expanded(child: Placeholder()),
+            Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: _firestore
+                    .collection('messages')
+                    .orderBy('dateCreated', descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if(!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  final messages = snapshot.data.docs;
+                  List<MessageBubble> messageBubbles = [];
+                  for(var message in messages) {
+                    final messageText = message.data()['text'];
+                    final messageSender = message.data()['sender'];
+
+                    final messageBubble = MessageBubble(
+                      sender: messageSender,
+                      text: messageText,
+                      isMyChat: messageSender == _activeUser.email,
+                    );
+                    messageBubbles.add(messageBubble);
+                  }
+                  return ListView(
+                    reverse: true,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 16,
+                    ),
+                    children: messageBubbles,
+                  );
+                },
+              ),
+            ),
             SizedBox(
               height: 8,
             ),
@@ -35,6 +101,7 @@ class ChatPage extends StatelessWidget {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: _messageTextController,
                     decoration: InputDecoration(
                       contentPadding:
                           EdgeInsets.symmetric(vertical: 8, horizontal: 16),
@@ -46,7 +113,9 @@ class ChatPage extends StatelessWidget {
                   width: 8,
                 ),
                 MaterialButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    _sendMessage();
+                  },
                   child: Text('SEND'),
                   color: Theme.of(context).primaryColor,
                   textTheme: ButtonTextTheme.primary,
@@ -58,4 +127,21 @@ class ChatPage extends StatelessWidget {
       ),
     );
   }
+  
+  void _sendMessage() {
+    _firestore.collection('messages').add({
+      'text' : _messageTextController.text,
+      'sender' : _activeUser.email,
+      'dateCreated' : Timestamp.now(),
+    });
+    _messageTextController.clear();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _messageTextController.dispose();
+    super.dispose();
+  }
+
 }
--- chat_app_dicoding/lib/widget/message_bubble.dart
+++ chat_app_dicoding/lib/widget/message_bubble.dart
@@ -4,13 +4,20 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   final String sender;
   final String text;
+  final bool isMyChat;
 
-  MessageBubble({this.sender, this.text});
+  MessageBubble({this.sender, this.text, this.isMyChat});
 
-  final bubbleBorderRadius = BorderRadius.only(
-    topRight: Radius.circular(20),
-    bottomRight: Radius.circular(20),
+  final senderBorderRadius = BorderRadius.only(
+    topLeft: Radius.circular(20),
     bottomLeft: Radius.circular(20),
+    topRight: Radius.circular(20),
+  );
+
+  final otherBorderRadius = BorderRadius.only(
+    topRight: Radius.circular(20),
+    topLeft: Radius.circular(20),
+    bottomRight: Radius.circular(20),
   );
 
   @override
@@ -18,7 +25,7 @@ class MessageBubble extends StatelessWidget {
     return Padding(
       padding: EdgeInsets.all(8),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: isMyChat ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(
             sender,
@@ -28,15 +35,15 @@ class MessageBubble extends StatelessWidget {
             ),
           ),
           Material(
-            color: Colors.white,
-            borderRadius: bubbleBorderRadius,
+            color: isMyChat ? Colors.lightBlue : Colors.white,
+            borderRadius: isMyChat ? senderBorderRadius : otherBorderRadius,
             elevation: 5,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
               child: Text(
                 text,
                 style: TextStyle(
-                  color: Colors.black54,
+                  color: isMyChat ? Colors.white : Colors.black54,
                   fontSize: 15,
                 ),
               ),
