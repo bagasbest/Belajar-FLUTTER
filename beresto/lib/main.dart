4ed48e6a1e09bc903dcfb93923d20e37909c3df4
bagasbest <bagasbest80@gmail.com>
2021-02-19 21:26:36 +0700

    remove unusued import

--- beresto/lib/main.dart
+++ beresto/lib/main.dart
@@ -1,23 +1,16 @@
 import 'dart:io';
 
 import 'package:android_alarm_manager/android_alarm_manager.dart';
-import 'package:beresto/api/api_service.dart';
 import 'package:beresto/provider/database_provider.dart';
 import 'package:beresto/provider/preferences_provider.dart';
-import 'package:beresto/provider/restaurant_provider.dart';
 import 'package:beresto/provider/schduling_provider.dart';
-import 'package:beresto/screen/favorite_resto_page.dart';
 import 'package:beresto/screen/home_page.dart';
-import 'package:beresto/screen/list_of_restautant.dart';
-import 'package:beresto/screen/settings_resto_page.dart';
 import 'package:beresto/service/background_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'helper/database_helper.dart';
 import 'helper/notification_helper.dart';
 import 'helper/preferences_helper.dart';
